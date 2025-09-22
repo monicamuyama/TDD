846d3008d4f0f5f10164794b56160ac5c06df9ef
monicamuyama <107669232+monicamuyama@users.noreply.github.com>
2025-09-22 09:49:12 +0300

    wrote all tests and they passed

--- test/test.js
+++ test/test.js
@@ -12,3 +12,18 @@ it("multiply 1 *1 ", () => {
 it("multiply 2 * 2 ", () => {
     assertEqual(multiply(2, 2), 4);
 });
+
+
+it("multiply 3 * 3 ", () => {
+    assertEqual(multiply(3, 3), 9);
+});
+
+
+it("multiply 4 * 4 ", () => {
+    assertEqual(multiply(4, 4), 16);
+});
+
+
+it("multiply 23 * 45 ", () => {
+    assertEqual(multiply(23, 45), 23*45);
+});
